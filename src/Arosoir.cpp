cd0eed669e30d16bb1407174289eb993286d6a8d
garyromain2@hotmail.fr <garyromain2@outlook.fr>
2020-05-20 14:00:22 +0200

    Mise a jour des fichier device.h et device.cpp en accord avec le diagramme de classe-2

---
+++ src/Arosoir.cpp
@@ -0,0 +1,59 @@
+#include "Arosoir.h"
+
+//Constructeur
+Arrosoir::Arrosoir():state(Arrete){
+}
+
+//Methodes liees au deplacement
+void Arrosoir::deplacerArrosoir(double positionPlante, Board* arduino){
+    //deplacement possible si l'on est pas en train d'arroser
+    if (state != EnArrosage){
+            //Si on est loin de la plante => on avance ou recule rapidement
+          if (arduino->analogRead(PIN_RADAR)<positionPlante-5){
+             arduino->digitalWrite(PIN_SERVO_ARROSOIR,VITESSE_ARROSOIR_AVANCE_RAPIDE);
+              state=EnDeplacement; //Mise a jour de l'etat de l'arrosoir
+          }
+          else if(arduino->analogRead(PIN_RADAR)>positionPlante+5){
+             arduino->digitalWrite(PIN_SERVO_ARROSOIR,VITESSE_ARROSOIR_RECULE_RAPIDE);
+              state=EnDeplacement; //Mise a jour de l'etat de l'arrosoir
+          }
+          //Si on s'approche de la plante, on va plus lentement
+          else if((arduino->analogRead(PIN_RADAR)<positionPlante-2)&&(arduino->analogRead(PIN_RADAR)>=positionPlante-5)){
+            arduino->digitalWrite(PIN_SERVO_ARROSOIR,VITESSE_ARROSOIR_AVANCE_LENT);
+             state=EnDeplacement; //Mise a jour de l'etat de l'arrosoir
+          }
+          else if((arduino->analogRead(PIN_RADAR)>positionPlante+2)&&(arduino->analogRead(PIN_RADAR)<=positionPlante+5)){
+            arduino->digitalWrite(PIN_SERVO_ARROSOIR,VITESSE_ARROSOIR_RECULE_LENT);
+             state=EnDeplacement; //Mise a jour de l'etat de l'arrosoir
+          }
+          //si on est à côté de la plante => arrêt
+          else if((arduino->analogRead(PIN_RADAR)<=positionPlante+2)||(arduino->analogRead(PIN_RADAR)>=positionPlante-2)){
+            arduino->digitalWrite(PIN_SERVO_ARROSOIR,VITESSE_ARROSOIR_ARRET);
+            state=Arrete; //Mise a jour de l'etat de l'arrosoir
+          }
+    }
+}
+
+//Methode liee a l'arrosage
+/*void Arrosoir::inclinerArrosoir(double angle,Board* arduino, AnalogActuatorServoInclinaison* servo){
+    //Arrosage possible uniquement si l'on est pas en deplacement
+    if (state != EnDeplacement){
+            //Si on a pas atteint l'inclinaison souhaitee, on continue a avancer
+        if ((servo->angle)<angle){
+            arduino->digitalWrite(PIN_SERVO_INCLINAISON,VITESSE_INCLINAISON_ON);
+        }
+        //Sinon on arrete le deplacement
+    else{
+        arduino->digitalWrite(PIN_SERVO_INCLINAISON,VITESSE_INCLINAISON_OFF);
+    }
+    //Mise a jour de l'etat de l'arrosoir
+    if (((servo->angle)==0)&&(state==EnArrosage)){ //si on a fini l'arrosage => l'arrosoir passe en arret
+            state=Arrete;
+    }
+    else if ((servo->angle)!=0) {  //si l'angle n'est pas à 0, on est en arrosage
+            state=EnArrosage;
+    }
+
+    }
+}*/
+
---
+++ src/Arosoir.h
@@ -0,0 +1,20 @@
+#ifndef ARROSOIR_H_INCLUDED
+#define ARROSOIR_H_INCLUDED
+#include "core_simulation.h"
+#include "mydevices.h"
+enum EtatArrosoir {Arrete, EnDeplacement, EnArrosage};
+//classe représentant l'arrosoir, contenant les méthodes permettant l'arrosage
+class Arrosoir{
+protected :
+    //Definit l'etat actuel de l'arrosoir (arrete, en deplacement ou en arrosage) => on ne peut pas arroser et se deplacer en meme temps
+    EtatArrosoir state;
+public:
+    //Constructeur
+    Arrosoir();
+    //methodes pour l'arrosage
+    void deplacerArrosoir(double positionPlante, Board* arduino);
+    void inclinerArrosoir(double angle, Board* arduino, AnalogActuatorServoInclinaison* servo);
+};
+
+
+#endif // ARROSOIR_H_INCLUDED
